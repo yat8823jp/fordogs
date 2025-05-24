4f715a76db603e502b446075a06af82e7ab820b4
y_kawanishi <yat@yat-net.com>
2025-05-24 18:32:15 +0900

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  function addDatas () {
+    
+  }
+
+  return (
+    <>
+      <h1>Fat calculator</h1>
+      <Form addDatas = { addDatas }/>
+    </>
+  )
+}
+
+export default App
