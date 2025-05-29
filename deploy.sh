20ee79a0c24f6cec7665c1360c8877436cad729d
y_kawanishi <yat@yat-net.com>
2025-05-29 18:56:26 +0900

    update deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+# 前回のビルドを削除
+rm -rf docs
+
+# ビルド実行
+bun run build 
+
+# 個別に必要なファイルをコピー
+cp docs/index.html docs/404.html
+
+# コミット & プッシュしてGitHubActionsをトリガー
+git add .
+git commit -m "deploy"
+git pull
+git push
